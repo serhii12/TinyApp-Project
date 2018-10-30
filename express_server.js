dfd85db5691a7e73a5c88c3fd11365f71bd8854f
Serhii Panchyshyn <panchyshyn.serhii@gmail.com>
2018-10-30 22:23:24 +0000

    added bootstap and fixed bugs inside delete function

--- express_server.js
+++ express_server.js
@@ -13,6 +13,7 @@ const generateRandomString = () => {
 };
 
 // view engine setup
+app.use(express.static(`${__dirname}/public`));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 
@@ -20,6 +21,14 @@ const urlDatabase = {
   b2xVnl: 'http://www.lighthouselabs.ca',
   '9sm5xK': 'http://www.google.com',
 };
+
+app.get('/', (req, res) => {
+  // if user is logged in:
+  //  (Minor) redirect to /urls
+  res.redirect(301, '/urls');
+  // if user is not logged in:
+  // (Minor) redirect to /login
+});
 
 app.get('/urls', (req, res) => {
   const templateVars = { urls: urlDatabase };
@@ -30,10 +39,12 @@ app.get('/urls/new', (req, res) => {
   res.render('pages/urls_new');
 });
 
-app.post('/urls', (req, res) => {
-  const getShortVersion = generateRandomString();
-  urlDatabase[getShortVersion] = req.body.longURL;
-  res.redirect(301, `/urls/${getShortVersion}`);
+app.get('/urls/:id', (req, res) => {
+  const templateVars = {
+    shortURL: req.params.id,
+    longURL: urlDatabase[req.params.id],
+  };
+  res.render('pages/urls_show', templateVars);
 });
 
 app.get('/u/:shortURL', (req, res) => {
@@ -41,12 +52,15 @@ app.get('/u/:shortURL', (req, res) => {
   res.redirect(longURL);
 });
 
-app.get('/urls/:id', (req, res) => {
-  const templateVars = {
-    shortURL: req.params.id,
-    longURL: urlDatabase[req.params.id],
-  };
-  res.render('pages/urls_show', templateVars);
+app.post('/urls', (req, res) => {
+  const getShortVersion = generateRandomString();
+  urlDatabase[getShortVersion] = req.body.longURL;
+  res.redirect(301, `/urls/${getShortVersion}`);
+});
+
+app.post('/urls/:id/delete', (req, res) => {
+  delete urlDatabase[req.params.id];
+  res.redirect('/urls');
 });
 
 app.listen(PORT, () => {
