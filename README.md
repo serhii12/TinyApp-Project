a8b8c0244c6cabcfb83955283db6e067d289b5a1
Serhii Panchyshyn <panchyshyn.serhii@gmail.com>
2018-10-30 18:36:05 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# TinyApp-Project
+
+TinyApp is a full stack web application built with Node and Express.
+The app will allow users to shorten long URLs much like TinyURL.com and bit.ly do.
+
+## Dependencies
+
+- Node.js
+- Express
+- EJS
+- bcrypt
+- body-parser
+- cookie-session
+- method-override
+
+## Getting Started
+
+- Install all dependencies (using the `npm install` command).
+- Run the development web server the `node express_server.js` command.
---
+++ express_server.js
@@ -0,0 +1,54 @@
+// load the things we need
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = 8080;
+
+const generateRandomString = () => {
+  const random = Math.random()
+    .toString(36)
+    .substring(7);
+  return random;
+};
+
+// view engine setup
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+
+const urlDatabase = {
+  b2xVnl: 'http://www.lighthouselabs.ca',
+  '9sm5xK': 'http://www.google.com',
+};
+
+app.get('/urls', (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render('pages/urls_index', templateVars);
+});
+
+app.get('/urls/new', (req, res) => {
+  res.render('pages/urls_new');
+});
+
+app.post('/urls', (req, res) => {
+  const getShortVersion = generateRandomString();
+  urlDatabase[getShortVersion] = req.body.longURL;
+  res.redirect(301, `/urls/${getShortVersion}`);
+});
+
+app.get('/u/:shortURL', (req, res) => {
+  const longURL = urlDatabase;
+  res.redirect(longURL);
+});
+
+app.get('/urls/:id', (req, res) => {
+  const templateVars = {
+    shortURL: req.params.id,
+    longURL: urlDatabase[req.params.id],
+  };
+  res.render('pages/urls_show', templateVars);
+});
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
